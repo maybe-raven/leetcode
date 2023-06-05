4aa0ebb490acc0e6da29d9311766cecbfe0c12c3
Raven <dsy1998@live.com>
2023-06-05 16:02:21 -0400

    347. Top K Frequent Elements
    Without `HashMap`.

--- src/top_k_frequent.rs
+++ src/top_k_frequent.rs
@@ -1,17 +1,35 @@
 //! 347. Top K Frequent Elements
 //! https://leetcode.com/problems/top-k-frequent-elements/
 
-use std::collections::HashMap;
-
 impl Solution {
-    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
-        for num in nums {
-            *map.entry(num).or_insert(0) += 1;
-        }
-        let mut vec = map.into_iter().collect::<Vec<_>>();
-        vec.sort_by(|a, b| b.1.cmp(&a.1));
-        vec.into_iter().take(k as usize).map(|x| x.0).collect()
+    pub fn top_k_frequent(mut nums: Vec<i32>, k: i32) -> Vec<i32> {
+        nums.sort_unstable();
+
+        let mut key = nums[0];
+        let mut count = 0;
+
+        let mut frequencies: Vec<(i32, i32)> = nums
+            .into_iter()
+            .filter_map(|num| {
+                if num == key {
+                    count += 1;
+                    None
+                } else {
+                    let result = (key, count);
+                    key = num;
+                    count = 1;
+                    Some(result)
+                }
+            })
+            .collect();
+        frequencies.push((key, count));
+        frequencies.sort_unstable_by(|a, b| b.1.cmp(&a.1));
+
+        frequencies
+            .into_iter()
+            .take(k as usize)
+            .map(|x| x.0)
+            .collect()
     }
 }
 
