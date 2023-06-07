7f4072820a9d42a15704a337252e84ea5318131b
Raven <dsy1998@live.com>
2023-06-06 20:11:57 -0400

    Fast.

--- src/three_sum.rs
+++ src/three_sum.rs
@@ -1,98 +1,101 @@
 //! 15. 3Sum
 //! https://leetcode.com/problems/3sum
 
-use std::collections::{HashMap, HashSet};
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-struct Triplet(i32, i32, i32);
-
-impl Triplet {
-    fn new(a: i32, b: i32, c: i32) -> Self {
-        let mut v0;
-        let mut v1;
-        let v2;
-
-        if a < b {
-            v0 = a;
-            v1 = b;
-        } else {
-            v0 = b;
-            v1 = a;
-        }
-
-        if c < v0 {
-            v2 = v1;
-            v1 = v0;
-            v0 = c;
-        } else if c < v1 {
-            v2 = v1;
-            v1 = c;
-        } else {
-            v2 = c;
-        }
-
-        Self(v0, v1, v2)
-    }
-}
-
-impl From<Triplet> for Vec<i32> {
-    fn from(value: Triplet) -> Self {
-        vec![value.0, value.1, value.2]
-    }
-}
-
-impl Triplet {
-    fn count(&self, value: i32) -> usize {
-        let mut result = 0;
-
-        if self.0 == value {
-            result += 1;
-        }
-
-        if self.1 == value {
-            result += 1;
-        }
-
-        if self.2 == value {
-            result += 1;
-        }
-
-        result
-    }
-}
+use std::collections::HashMap;
 
 impl Solution {
-    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let memo = {
-            let mut memo: HashMap<i32, usize> = HashMap::new();
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let counter = {
+            let mut counter: HashMap<i32, usize> = HashMap::new();
 
-            for num in nums {
-                memo.entry(num).and_modify(|v| *v += 1).or_insert(1);
+            for num in nums.drain(..) {
+                counter.entry(num).and_modify(|v| *v += 1).or_insert(1);
             }
 
-            memo
+            counter
         };
 
-        let mut result: HashSet<Triplet> = HashSet::new();
-        for (&num0, &count0) in memo.iter() {
-            for (&num1, &count1) in memo.iter() {
-                let num2 = -num0 - num1;
-                if let Some(&count2) = memo.get(&num2) {
-                    let triplet = Triplet::new(num0, num1, num2);
+        nums.extend(counter.keys());
+        nums.sort_unstable();
 
-                    if count0 < triplet.count(num0)
-                        || count1 < triplet.count(num1)
-                        || count2 < triplet.count(num2)
-                    {
+        // `Vec::partition_point` finds the index of the first element
+        // where the predicate returns false,
+        // which means the return index could point to zero,
+        // or it could point to the first positive number in the vec,
+        // if zero doesn't exist.
+        let neg_end = nums.partition_point(|&x| x < 0);
+
+        // This means the input is all negative numbers,
+        // and you can't sum to 0 with only negative numbers.
+        if neg_end == nums.len() {
+            return vec![];
+        }
+
+        let mut results = Vec::new();
+
+        // 0 can only appear once or thrice in a triplet.
+        // [0, 0, n] where n != 0 is never valid.
+        // Here we check the possibility of [0, 0, 0] specifically.
+        let pos_start = if nums[neg_end] == 0 {
+            if counter[&0] >= 3 {
+                results.push(vec![0, 0, 0]);
+            }
+
+            neg_end + 1
+        } else {
+            neg_end
+        };
+
+        // `neg_end == 0` means the input is all non-negative numbers,
+        // you can't sum to 0 with only positive numbers,
+        // so the only way to sum to 0 would be [0, 0, 0],
+        // which we've already checked.
+        if neg_end == 0 {
+            return results;
+        }
+
+        // We can be sure that `(neg_num, pos_num)` is unique every iteration,
+        // because `nums` only contain unique values,
+        // and there is no overlap between range `..neg_end` and `pos_start..`.
+
+        // However, `(neg_num, pos_num, offset_num)` is not guaranteed to be unique.
+        // Consider a, b, c where `a < 0 < b < c`, and `a + b + c == 0`,
+        // since both b and c are positive, they are both in `nums[pos_start..]`,
+        // that means that there will be an iteration
+        // where `pos_num == b`, and an iteration where `pos_num == c`,
+        // and `offset_num == -(a + b) == c` and `offset_num == -(a + c) == b`, respectively.
+        // Example: `a=-3`, `b=1`, `c=2` will make both `(-3, 1, 2)` and `(-3, 2, 1)`.
+
+        // To prevent duplicates from occuring this way,
+        // we can choose to discard any iterations where `offset_num == c`,
+        // that is to say, we require `neg_num <= offset_num <= pos_num`.
+        for &neg_num in &nums[..neg_end] {
+            for &pos_num in &nums[pos_start..] {
+                let offset_num = -(neg_num + pos_num);
+
+                // `offset_num < neg_num` == `-neg_num - pos_num < neg_num` == `2 * abs(neg_num) < pos_num`
+                // and since `pos_num` only goes up at every iteration of the inner loop,
+                // once `2 * abs(neg_num) < pos_num` becomes true,
+                // it will continue to be true until the next outer iteration.
+                if offset_num < neg_num {
+                    break;
+                }
+
+                if offset_num > pos_num {
+                    continue;
+                }
+
+                if let Some(&offset_num_count) = counter.get(&offset_num) {
+                    if (offset_num == neg_num || offset_num == pos_num) && offset_num_count < 2 {
                         continue;
                     }
 
-                    result.insert(triplet);
+                    results.push(vec![neg_num, pos_num, offset_num]);
                 }
             }
         }
 
-        result.into_iter().map(Vec::<i32>::from).collect()
+        results
     }
 }
 
@@ -102,29 +105,41 @@ pub struct Solution;
 mod tests {
     use super::*;
 
+    fn do_test_and_assert(input: Vec<i32>, expected_num_results: usize) {
+        let mut results = Solution::three_sum(input);
+        results.iter_mut().for_each(|triplet| {
+            assert_eq!(0, triplet.iter().sum::<i32>());
+            triplet.sort_unstable();
+        });
+        results.sort_unstable();
+        results.dedup();
+        assert_eq!(results.len(), expected_num_results);
+    }
+
     #[test]
     fn test_three_sum() {
-        assert!({
-            let result = Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]);
-            result.contains(&vec![-1, -1, 2]) && result.contains(&vec![-1, 0, 1])
-        });
-        assert!({
-            let result = Solution::three_sum(vec![
+        do_test_and_assert(
+            vec![
                 12750, 99982, -84572, -89660, -14691, -76700, 51601, 3300, 40608, -30604, 46884,
                 -6139, -52495, -98856, -17703, 4267, -47010, -94633, -44825, -8252, 1757, 77027,
                 -87822, 11051, 10341, 55353, 22098, 41130, 77985, -69890, 1872, 13436,
-            ]);
-            result.contains(&vec![-17703, 4267, 13436]) && result.contains(&vec![-6139, 1872, 4267])
-        });
-        assert!(Solution::three_sum(vec![0, 1, 1]).is_empty());
-        assert_eq!(Solution::three_sum(vec![0, 0, 0]), vec![vec![0, 0, 0]]);
+            ],
+            2,
+        );
+        do_test_and_assert(vec![-1, 0, 1, 2, -1, -4], 2);
+        do_test_and_assert(vec![3, 0, -2, -1, 1, 2], 3);
+        do_test_and_assert(
+            vec![-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0],
+            6,
+        );
+        do_test_and_assert(vec![0, 1, 1], 0);
+        do_test_and_assert(vec![0, 0, 0], 1);
     }
 
     #[test]
     fn test_three_sum_large_input() {
-        assert_eq!(
-            vec![vec![0, 0, 0]],
-            Solution::three_sum(vec![
+        do_test_and_assert(
+            vec![
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -232,8 +247,9 @@ mod tests {
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0
-            ])
+                0, 0, 0, 0,
+            ],
+            1,
         );
     }
 }
