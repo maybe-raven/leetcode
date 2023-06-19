026c5afc6c1569e4f65e69d7080b5678a55a7905
Raven <dsy1998@live.com>
2023-06-19 00:18:59 -0400

    Quick save.

--- src/make_array_increasing.rs
+++ src/make_array_increasing.rs
@@ -1,42 +1,12 @@
 //! 1187. Make Array Strictly Increasing
 //! https://leetcode.com/problems/make-array-strictly-increasing
 
-use std::{
-    cmp::{max, min},
-    ops::Sub,
-};
-
-trait IsNoneOr<T>: Copy {
-    fn is_none_or(self, f: impl FnOnce(T) -> bool) -> bool;
-}
-
-impl<T: Copy> IsNoneOr<T> for Option<T> {
-    fn is_none_or(self, f: impl FnOnce(T) -> bool) -> bool {
-        match self {
-            Some(x) => f(x),
-            None => true,
-        }
-    }
-}
+use std::cell::RefCell;
 
 #[derive(Debug, Clone, Copy)]
-enum Index {
-    Inclusive(usize),
-    Exclusive(usize),
-}
-
-impl Sub for Index {
-    type Output = usize;
-
-    #[allow(clippy::suspicious_arithmetic_impl)]
-    fn sub(self, rhs: Self) -> Self::Output {
-        match (self, rhs) {
-            (Index::Exclusive(lhs), Index::Inclusive(rhs))
-            | (Index::Inclusive(lhs), Index::Inclusive(rhs)) => lhs.saturating_sub(rhs),
-            (Index::Inclusive(lhs), Index::Exclusive(rhs))
-            | (Index::Exclusive(lhs), Index::Exclusive(rhs)) => lhs.saturating_sub(rhs + 1),
-        }
-    }
+enum Number {
+    Original(i32),
+    Replaced(usize),
 }
 
 impl Solution {
@@ -48,95 +18,58 @@ impl Solution {
         arr2.sort_unstable();
         arr2.dedup();
 
-        // Build a "map" of indices of replacement for every number in `arr1`.
-        //
-        // For all `i` where `0 <= i < arr1.len()`,
-        // we have `arr2[..indexer[i]] < arr1[i] <= arr2[indexer[i]..]`.
-        //
-        // So given `i` and `j` where `0 <= i < j < arr1.len()`,
-        // we have `arr1[i] <= arr2[indexer[i]..indexer[j]] < arr1[j]`.
-        // Or alternatively, `arr1[i] < arr2[k..indexer[j]] < arr1[j]`,
-        // where `k = indexer[i]` if `arr1[i] != arr2[indexer[i]]` else `k = indexer[i] + 1`.
-        // From this, we can know there are `indexer[j] - k` numbers in `arr2` that
-        // fit between `arr1[i]` and `arr1[j]`.
-        let indexer: Vec<Index> = arr1
-            .iter()
-            .map(|x| match arr2.binary_search(x) {
-                Ok(i) => Index::Exclusive(i),
-                Err(i) => Index::Inclusive(i),
-            })
-            .collect();
+        let memo: Vec<RefCell<Vec<(Number, usize)>>> = vec![RefCell::new(Vec::new()); arr1.len()];
 
-        solutionate(&arr1, &indexer, arr2.len(), 0).map_or(-1, |x| x as i32)
-    }
-}
-
-/// Find the mininum number of swicheroos needed to make `arr1[narnia..]` strictly increasing,
-/// without touching `arr1[..narnia]`, as that part is narnia business.
-fn solutionate(
-    arr1: &[i32],
-    indexonomicon: &[Index],
-    arr2_len: usize,
-    narnia: usize,
-) -> Option<usize> {
-    for (i, window) in arr1.windows(2).enumerate().skip(narnia) {
-        let &[a, b] = window else { unreachable!() };
-
-        if a < b {
-            continue;
+        memo[0].borrow_mut().push((Number::Original(arr1[0]), 0));
+        if arr1[0] != arr2[0] {
+            memo[0].borrow_mut().push((Number::Replaced(0), 1));
         }
 
-        let mut mintendo_switch = None;
+        for (memo_window, x) in memo.windows(2).zip(arr1) {
+            let [previous_states, current_states] = memo_window else { unreachable!() };
 
-        for switcheroo_size in 1..=min(arr1.len() - narnia, arr2_len) {
-            // `end - start = n`; `n` is the length of the slicing.
-            // `arr1[start..end]` is the slice that we're looking to replace in order to fix
-            // it's ordering. So it can only be as long as the shorter of `arr1` and `arr2`.
+            for &state in previous_states.borrow().iter() {
+                match state {
+                    (Number::Original(y), swap_count) => {
+                        if y < x {
+                            current_states
+                                .borrow_mut()
+                                .push((Number::Original(x), swap_count));
+                        }
 
-            // We want as small an `n` as possible, so we want to explore all the possible
-            // slices that contain either `a` or `b`, or both.
-            // We want all possible `start` and `end` given `n`,
-            // where `narnia <= start <= i + 1` (starting on `b` or before),
-            // and `arr1.len() >= end >= i + 1` (ending on and including `a` or after).
+                        if let Some(i) = match arr2.binary_search(&y) {
+                            Ok(i) | Err(i) if i < arr2.len() - 1 => Some(i + 1),
+                            Ok(_) | Err(_) => None,
+                        } {
+                            current_states
+                                .borrow_mut()
+                                .push((Number::Replaced(i), swap_count + 1))
+                        }
+                    }
+                    (Number::Replaced(mut i), swap_count) => {
+                        if arr2[i] < x {
+                            current_states
+                                .borrow_mut()
+                                .push((Number::Original(x), swap_count));
+                        }
 
-            // Since `end = start + n` and `start <= i + 1`, `end <= i + 1 + n`.
-            // We want `start >= narnia`; we have `start = end - n` and `end >= i + 1`,
-            // therefore we want `i + 1 - n >= narnia`, therefore `i + 1 >= n + narnia`
-            for end in
-                max(switcheroo_size + narnia, i + 1)..=min(i + switcheroo_size + 1, arr1.len())
-            {
-                let start = end - switcheroo_size;
-
-                let min_index = if start == 0 {
-                    Index::Inclusive(0)
-                } else {
-                    indexonomicon[start - 1]
-                };
-
-                let max_index = if end == indexonomicon.len() {
-                    Index::Inclusive(arr2_len)
-                } else {
-                    indexonomicon[end]
-                };
-
-                if switcheroo_size <= max_index - min_index {
-                    let Some(switcheroo_size) = solutionate(arr1, indexonomicon, arr2_len, end)
-                        .map(|n| n + switcheroo_size)
-                    else {
-                        continue;
-                    };
-
-                    if mintendo_switch.is_none_or(|x| switcheroo_size < x) {
-                        mintendo_switch = Some(switcheroo_size);
+                        i += 1;
+                        if i < arr2.len() {
+                            current_states
+                                .borrow_mut()
+                                .push((Number::Replaced(i), swap_count + 1));
+                        }
                     }
                 }
+
+                unimplemented!()
             }
+
+            todo!()
         }
 
-        return mintendo_switch;
+        unimplemented!()
     }
-
-    Some(0)
 }
 
 pub struct Solution;
@@ -163,7 +96,7 @@ mod tests {
         );
         assert_eq!(
             2,
-            Solution::make_array_increasing(vec![1, 5, 3, 6, 7], vec![4, 3, 1])
+            Solution::make_array_increasing(vec![1, 5, 3, 6, 7], vec![4, 3, 1]) // [1, *3*, *4*, 6, 7]
         );
         assert_eq!(
             -1,
