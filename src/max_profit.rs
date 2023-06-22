6083a198d0dd5e049079219416a0c2acafa74956
Raven <dsy1998@live.com>
2023-06-22 15:59:13 -0400

    Stonks 2: Electric Boogaloo

--- src/max_profit.rs
+++ src/max_profit.rs
@@ -1,74 +1,39 @@
 //! 714. Best Time to Buy and Sell Stock with Transaction Fee
 //! https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
 
-use std::cmp::Ordering;
-
-trait IsNoneOr<T>: Copy {
-    fn is_none_or(self, f: impl FnOnce(T) -> bool) -> bool;
-}
-
-impl<T: Copy> IsNoneOr<T> for Option<T> {
-    fn is_none_or(self, f: impl FnOnce(T) -> bool) -> bool {
-        match self {
-            Some(x) => f(x),
-            None => true,
-        }
-    }
-}
-
 impl Solution {
     pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
-        let mut memo: Vec<(i32, Option<i32>, Option<i32>)> = vec![(0, None, None)];
-        let mut extender: Vec<(i32, Option<i32>, Option<i32>)> = Vec::new();
+        let mut profit = 0;
+        let mut buy_price = None;
+        let mut sell_price: Option<i32> = None;
 
-        // println!("{:?}", &prices);
         for &price in &prices {
-            for (profit, buy_price, sell_price) in memo.iter_mut() {
-                let buy = buy_price.get_or_insert(price);
-                if price < *buy {
-                    if let Some(sell) = sell_price {
-                        *profit += *sell - *buy - fee;
-                        *sell_price = None;
-                    }
-                    *buy = price;
-                } else if price > *buy + fee && sell_price.is_none_or(|x| price > x) {
-                    *sell_price = Some(price);
-                } else if let Some(sell) = sell_price {
-                    extender.push(((*profit + *sell - *buy - fee), Some(price), None));
+            let buy = buy_price.get_or_insert(price);
+            if price < *buy {
+                if let Some(sell) = sell_price {
+                    profit += sell - *buy - fee;
+                    sell_price = None;
                 }
+                *buy = price;
+            } else if let Some(sell) = sell_price {
+                if price > sell {
+                    sell_price = Some(price);
+                    continue;
+                } else if sell > price + fee {
+                    profit += sell - *buy - fee;
+                    *buy = price;
+                    sell_price = None;
+                }
+            } else if price > *buy + fee {
+                sell_price = Some(price);
             }
-
-            memo.append(&mut extender);
-            memo.sort_unstable_by(|(p0, b0, s0), (p1, b1, s1)| match b0.cmp(b1) {
-                // Buy price in increasing order
-                Ordering::Equal => match s1.cmp(s0) {
-                    // Sell price in decreasing order.
-                    Ordering::Equal => p1.cmp(p0), // Profit in decreasing order.
-                    ordering => ordering,
-                },
-                ordering => ordering,
-            });
-            memo.dedup_by_key(|&mut (_, buy_price, sell_price)| (buy_price, sell_price));
-
-            // println!("price: {}", price);
-            // for (profit, buy_price, sell_price) in memo.iter() {
-            //     println!(
-            //         "profit: {}; buy: {:?}; sell: {:?}",
-            //         profit, buy_price, sell_price
-            //     );
-            // }
         }
 
-        memo.into_iter()
-            .map(|(profit, buy_price, sell_price)| {
-                if let (Some(buy), Some(sell)) = (buy_price, sell_price) {
-                    profit + sell - buy - fee
-                } else {
-                    profit
-                }
-            })
-            .max()
-            .unwrap()
+        if let (Some(buy), Some(sell)) = (buy_price, sell_price) {
+            profit + sell - buy - fee
+        } else {
+            profit
+        }
     }
 }
 
