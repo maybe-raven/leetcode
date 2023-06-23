1f843ec44a00b8491a84ac32c02e5bd008410fe6
Raven <dsy1998@live.com>
2023-06-22 22:30:43 -0400

    Impl: Get a dumb solution real quick.

--- src/longest_arith_seq_length.rs
+++ src/longest_arith_seq_length.rs
@@ -3,8 +3,40 @@
 
 impl Solution {
     pub fn longest_arith_seq_length(nums: Vec<i32>) -> i32 {
-        unimplemented!()
+        let mut max_count = 0;
+
+        for (i, &a) in nums.iter().enumerate() {
+            for (j, &b) in nums.iter().enumerate().skip(i + 1) {
+                let step = a - b;
+                let mut count = 2;
+                let mut expected = b - step;
+                for &c in &nums[j + 1..] {
+                    if c == expected {
+                        count += 1;
+                        expected -= step;
+                    }
+                }
+                max_count = max_count.max(count);
+            }
+        }
+
+        max_count
     }
 }
 
 pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        assert_eq!(4, Solution::longest_arith_seq_length(vec![3, 6, 9, 12]));
+        assert_eq!(3, Solution::longest_arith_seq_length(vec![9, 4, 7, 2, 10]));
+        assert_eq!(
+            4,
+            Solution::longest_arith_seq_length(vec![20, 1, 15, 3, 10, 5, 8])
+        );
+    }
+}
