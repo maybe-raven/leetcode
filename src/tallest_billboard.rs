ea78875e08d618d6680d171b43b1d351358eaa08
Raven <dsy1998@live.com>
2023-06-23 22:30:34 -0400

    Impl: `[u8; 20]` is again much faster than `Vec<usize>`.

--- src/tallest_billboard.rs
+++ src/tallest_billboard.rs
@@ -1,22 +1,38 @@
 //! 956. Tallest Billboard
 //! https://leetcode.com/problems/tallest-billboard
 
-use std::collections::{btree_map::Entry, BTreeMap};
+use std::collections::BTreeMap;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct SmallIndex(u8);
+
+impl SmallIndex {
+    const INVALID_VALUE: u8 = 100;
+    const INVALID: Self = Self(Self::INVALID_VALUE);
+
+    fn is_invalid(self) -> bool {
+        self.0 == Self::INVALID_VALUE
+    }
+}
+
 impl Solution {
     pub fn tallest_billboard(mut rods: Vec<i32>) -> i32 {
         let half_sum = rods.iter().sum::<i32>() / 2;
 
         rods.sort_unstable();
-        let mut memo: BTreeMap<i32, Vec<_>> = BTreeMap::new();
+        let mut memo: BTreeMap<i32, Vec<[SmallIndex; 20]>> = BTreeMap::new();
 
         for (i, &x) in rods.iter().enumerate() {
             if x > half_sum {
                 break;
             }
 
+            let i = i as u8;
+            let mut inner = [SmallIndex::INVALID; 20];
+            inner[0] = SmallIndex(i);
             memo.entry(x)
-                .and_modify(|v| v.push(vec![i]))
-                .or_insert_with(|| vec![vec![i]]);
+                .and_modify(|v| v.push(inner))
+                .or_insert_with(|| vec![inner]);
 
             for (&k, v) in memo.clone().iter() {
                 let sum = x + k;
@@ -28,24 +44,24 @@ impl Solution {
                 let mut v = v
                     .iter()
                     .filter_map(|s| {
-                        if s.contains(&i) {
+                        if s.contains(&SmallIndex(i)) {
                             None
                         } else {
                             let mut s = s.clone();
-                            s.push(i);
+                            for j in s.iter_mut() {
+                                if j.is_invalid() {
+                                    *j = SmallIndex(i);
+                                    break;
+                                }
+                            }
                             Some(s)
                         }
                     })
                     .collect();
 
-                match memo.entry(sum) {
-                    Entry::Vacant(entry) => {
-                        entry.insert(v);
-                    }
-                    Entry::Occupied(mut entry) => {
-                        entry.get_mut().append(&mut v);
-                    }
-                }
+                memo.entry(sum)
+                    .and_modify(|v0| v0.append(&mut v))
+                    .or_insert(v);
             }
         }
 
@@ -53,7 +69,9 @@ impl Solution {
             .rev()
             .find_map(|(k, v)| {
                 for s0 in &v {
-                    if v.iter().any(|s1| !s1.iter().any(|x| s0.contains(x))) {
+                    if v.iter()
+                        .any(|s1| !s0.iter().any(|x| x.is_invalid() ^ s1.contains(x)))
+                    {
                         return Some(k);
                     }
                 }
@@ -74,6 +92,7 @@ mod tests {
     fn test_solution() {
         assert_eq!(6, Solution::tallest_billboard(vec![1, 2, 3, 6]));
         assert_eq!(10, Solution::tallest_billboard(vec![1, 2, 3, 4, 5, 6]));
+        assert_eq!(6, Solution::tallest_billboard(vec![3, 4, 3, 3, 2]));
         assert_eq!(0, Solution::tallest_billboard(vec![1, 2]));
         assert_eq!(
             0,
