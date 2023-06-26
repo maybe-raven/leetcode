0bf1c51ec8f65ad6b09f73452c3e11b626f8739f
Raven <dsy1998@live.com>
2023-06-26 00:14:19 -0400

    Impl: Slight optimization, but still way too slow.

--- src/total_cost.rs
+++ src/total_cost.rs
@@ -1,49 +1,38 @@
 //! 2462. Total Cost to Hire K Workers
 //! https://leetcode.com/problems/total-cost-to-hire-k-workers
 
-use std::{
-    cmp::Ordering,
-    ops::{Deref, IndexMut},
-};
-
-fn f<A, B: Copy, C: Deref<Target = Option<B>>>((a, c): (A, C)) -> Option<(A, B)> {
-    if let Some(b) = *c {
-        Some((a, b))
-    } else {
-        None
-    }
-}
+use std::{cmp::Ordering, ops::IndexMut};
 
 impl Solution {
-    pub fn total_cost(costs: Vec<i32>, k: i32, candidates: i32) -> i64 {
+    pub fn total_cost(mut costs: Vec<i32>, k: i32, candidates: i32) -> i64 {
         assert!(costs.len() >= k as usize);
 
-        let candidates = candidates as usize;
-        let mut costs: Vec<Option<i32>> = costs.into_iter().map(Some).collect();
+        let mut lc = candidates as usize;
+        let mut rc = lc;
         let mut total = 0;
+
         for _ in 0..k {
             let i = costs
                 .iter()
                 .enumerate()
-                .filter_map(f)
-                .take(candidates)
-                .chain(
-                    costs
-                        .iter()
-                        .enumerate()
-                        .rev()
-                        .filter_map(f)
-                        .take(candidates),
-                )
+                .take(lc)
+                .chain(costs.iter().enumerate().rev().take(rc))
                 .min_by(|(i, a), (j, b)| match a.cmp(b) {
                     Ordering::Equal => i.cmp(j),
                     ordering => ordering,
                 })
                 .expect("costs.len >= k")
                 .0;
+
             let c = costs.index_mut(i);
-            total += c.expect("`f` filters `None` values.") as i64;
-            *c = None;
+            total += *c as i64;
+            *c = i32::MAX;
+
+            if i <= lc {
+                lc += 1;
+            } else {
+                rc += 1;
+            }
         }
 
         total
