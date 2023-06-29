9c34c569bd881f4ac29aa48ce00aa1f6a0f768e8
Raven <dsy1998@live.com>
2023-06-28 21:14:28 -0400

    add: skeleton for 864. Shortest Path to Get All Keys

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ pub mod max_value;
 pub mod min_cost;
 pub mod minimum_difference_bst;
 pub mod num_of_ways;
+pub mod shortest_path_all_keys;
 pub mod shortest_path_binary_matrix;
 pub mod snapshot_array;
 pub mod solve_sudoku;
---
+++ src/shortest_path_all_keys.rs
@@ -0,0 +1,10 @@
+//! 864. Shortest Path to Get All Keys
+//! https://leetcode.com/problems/shortest-path-to-get-all-keys
+
+impl Solution {
+    pub fn shortest_path_all_keys(grid: Vec<String>) -> i32 {
+        unimplemented!()
+    }
+}
+
+pub struct Solution;
